607d71b31646cd034d5c875b0d9c24d9ddad51a7
Connor Mendenhall <ecmendenhall@gmail.com>
2013-02-15 23:08:57 -0700

    Use lein 2.0

--- project.clj
+++ project.clj
@@ -10,6 +10,7 @@
                  [jayq "2.2.0"]]
   :plugins [[lein-ring "0.8.2"]
             [lein-cljsbuild "0.3.0"]]
+  :min-lein-version "2.0.0"
   :cljsbuild {
     :builds [{
       :source-paths ["src/partial_applications/cljs"]
